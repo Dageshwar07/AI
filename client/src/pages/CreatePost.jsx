170a1b188af56fc5d3d8eea48e9b03498707add4
Dageshwar07 <dageshwardasmanikpuri996@gmail.com>
2023-02-07 18:35:15 +0530

    07-02-2023

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://localhost:3000/api/v1/post', {
+        const response = await fetch('http://localhost:3000/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
